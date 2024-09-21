2e098b73b5a2d0c7466d5560001a28de9bd84cce
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-21 22:25:46 +0200

    :heavy_plus_sign: UI for the level editor scene.

---
+++ levelCreator/LevelEditorUI.cpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2024
+** RType - Level Creator
+** File description:
+** LevelEditorUI.hpp file
+*/
+
+#include "LevelEditorUI.hpp"
+#include "UIButton.hpp"
+#include "UISprite.hpp"
+#include <optional>
+#include <functional>
+
+void LevelEditorUI::initUI()
+{
+    addElement(std::make_shared<UISprite>(
+        sf::Vector2f(1920 / 2, 1080 / 2),
+        sf::Vector2f(1, 1),
+        "./assets/bg2.png",
+        std::nullopt,
+        std::nullopt,
+        0));
+    addElement(std::make_shared<UIButton>(
+        sf::Vector2f(50, 50),
+        sf::Vector2f(200, 50),
+        "Menu",
+        sf::Color(150, 150, 150, 255),
+        std::function<void(IUIElement &)>([this](IUIElement &element) {
+            UIButton &button = dynamic_cast<UIButton &>(element);
+            button.setColor(sf::Color(100, 100, 100, 255));
+            __menu = true;
+        }),
+        std::function<void(IUIElement &)>([](IUIElement &element) {
+            UIButton &button = dynamic_cast<UIButton &>(element);
+            button.setColor(sf::Color(125, 125, 125, 255));
+        }),
+        2));
+}
+
+int LevelEditorUI::handleEvents(sf::Event &event, sf::Vector2f &worldMousePosition)
+{
+    if (event.type == sf::Event::KeyPressed)
+        if (event.key.code == sf::Keyboard::L)
+            return 1;
+    for (auto &element : getElements()) {
+        if (element->handleEvents(event, worldMousePosition))
+            return 2;
+    }
+    if (__menu)
+        return 4;
+    return 0;
+}
---
+++ levelCreator/LevelEditorUI.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2024
+** RType - Level Creator
+** File description:
+** LevelEditorUI.hpp file
+*/
+
+#ifndef LEVELEDITORUI_HPP
+    #define LEVELEDITORUI_HPP
+
+    #include "AUI.hpp"
+    #include "Window.hpp"
+    #include <iostream>
+    #include <vector>
+
+class LevelEditorUI : public AUI {
+    public:
+        LevelEditorUI(int cellSize, int mapSizeX, int mapSizeY, int startingZoom) :
+            __cellSize(cellSize),
+            __mapSizeX(mapSizeX),
+            __mapSizeY(mapSizeY),
+            __startingZoom(startingZoom),
+            __menu(false) {};
+        void initUI() override;
+
+        int handleEvents(sf::Event &event, sf::Vector2f &worldMousePosition) override;
+
+    private:
+
+        int __cellSize;
+        int __mapSizeX;
+        int __mapSizeY;
+        int __startingZoom;
+        bool __menu;
+};
+
+#endif // LEVELEDITORUI_HPP
