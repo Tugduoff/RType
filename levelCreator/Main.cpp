ef02bd77620e87ed22d741845e8dc0da6cbb1cf3
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-21 22:21:30 +0200

    :heavy_plus_sign: Main file for the level Creator project.

---
+++ levelCreator/Main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** RType - Level Creator
+** File description:
+** Main.cpp file
+*/
+
+#include "Game.hpp"
+
+int main()
+{
+    Game game;
+
+    game.run();
+    return 0;
+}
