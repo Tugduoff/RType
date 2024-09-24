e9ef028636d1e476664e159621bbf76747925f34
tibaud le maout <tibaud.le-maout@epitech.eu>
2024-09-24 18:37:06 +0200

    :heavy_plus_sign: Added a layer aspect to position to better handle the overlapping of sprites in the future

--- plugins/components/position/Position.hpp
+++ plugins/components/position/Position.hpp
@@ -15,13 +15,14 @@
 
 class Position : public AComponent {
     public:
-        Position() : x(0), y(0) {};
-        Position(uint32_t x, uint32_t y) : x(x), y(y) {};
+        Position() : x(0), y(0), layer(0) {};
+        Position(uint32_t x, uint32_t y) : x(x), y(y), layer(layer) {};
         ~Position() = default;
 
         std::vector<uint8_t> serialize() override {
             __network.x = htonl(x);
             __network.y = htonl(y);
+            __network.layer = htonl(layer);
             return std::vector<uint8_t>(__data, __data + sizeof(__data));
         };
 
@@ -30,19 +31,22 @@ class Position : public AComponent {
                 throw std::runtime_error("Invalid data size for position component");
             x = ntohl(*reinterpret_cast<uint32_t *>(data.data()));
             y = ntohl(*reinterpret_cast<uint32_t *>(data.data() + 4));
+            layer = ntohl(*reinterpret_cast<uint32_t *>(data.data() + 8));
         };
 
         size_t getSize() const override { return sizeof(__data); };
 
         uint32_t x;
         uint32_t y;
+        uint32_t layer;
     private:
         union {
             struct {
                 uint32_t x;
                 uint32_t y;
+                uint32_t layer;
             } __network;
-            uint8_t __data[8];
+            uint8_t __data[12];
         };
 };
 
