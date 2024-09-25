5197b689f96098d334e399d280a58e423c8b7dbe
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-25 12:58:12 +0200

    :heavy_plus_sign: :truck: Moved entitymanager class and entity class to entity subfolder of ECS.

---
+++ ECS/entity/EntityManager.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** EntityManager.cpp file
+*/
+
+#include "EntityManager.hpp"
+
+ECS::Entity ECS::EntityManager::spawnEntity()
+{
+    __entities.push_back(Entity(__nextEntityId++));
+    return __entities.back();
+}
+
+ECS::Entity ECS::EntityManager::entityFromIndex(size_t idx)
+{
+    if (idx >= __entities.size())
+        throw std::runtime_error("Entity index out of range");
+    return __entities[idx];
+}
+
+void ECS::EntityManager::killEntity(Entity const &e)
+{
+    __entities.erase(std::remove(__entities.begin(), __entities.end(), e), __entities.end());
+}
---
+++ ECS/entity/EntityManager.hpp
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** EntityManager.hpp file
+*/
+
+#ifndef ENTITY_MANAGER_HPP
+    #define ENTITY_MANAGER_HPP
+
+    #include "Entity.hpp"
+    #include <algorithm>
+    #include <stdexcept>
+    #include <vector>
+
+namespace ECS {
+    /**
+     * @class EntityManager
+     * 
+     * @brief Class that manages entities inside the ECS
+     */
+    class EntityManager {
+        public:
+
+            /**
+             * @brief Spawn a new entity
+             */
+            Entity spawnEntity();
+
+            /**
+             * @brief Get an entity from its index
+             * 
+             * @param idx : the index
+             * 
+             * @return Entity
+             */
+            Entity entityFromIndex(size_t idx);
+
+            /**
+             * @brief Kill an entity
+             * 
+             * @param e : the entity
+             */
+            void killEntity(Entity const &e);
+
+        private:
+
+            std::vector<Entity> __entities;
+            size_t __nextEntityId = 0;
+    };
+};
+
+#endif // ENTITY_MANAGER_HPP
