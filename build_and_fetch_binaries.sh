d68fe6c4763cd09760ff6d2948b95e04b2c17808
Clément Baziret <clement.baziret@epitech.eu>
2024-09-27 21:44:21 +0200

    :heavy_plus_sign: Added the ubuntu compilation

--- build_and_fetch_binaries.sh
+++ build_and_fetch_binaries.sh
@@ -1,7 +1,18 @@
-docker build --file FedoraDockerfile -t fedora_compilation .
-docker container rm fedora_build -f
+# Fedora Compilation
+
+docker build --file FedoraDockerfile -t fedora_compilation . --progress plain
+docker container rm fedora_build -f > /dev/null
 docker run --name fedora_build fedora_compilation
 
 rm -f ./fedora_build.tar
-
 docker cp fedora_build:/app/fedora_build.tar .
+
+
+# Ubuntu Compilation
+
+docker build --file UbuntuDockerfile -t ubuntu_compilation . --progress plain
+docker container rm ubuntu_build -f > /dev/null
+docker run --name ubuntu_build ubuntu_compilation
+
+rm -f ./ubuntu_build.tar
+docker cp ubuntu_build:/app/ubuntu_build.tar .
