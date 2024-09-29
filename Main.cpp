8d31827dff9c6669ba16b1bad8a49892858c4cc2
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-29 19:17:13 +0200

    :heavy_minus_sign: Removed console logs.

--- Main.cpp
+++ Main.cpp
@@ -21,21 +21,19 @@ int main() {
         ECS::Registry &reg = engine.getRegistry();
         ECS::Entity entity = reg.entityManager().spawnEntity();
 
-        // engine.loadSystems("./plugins/bin/systems/", "./plugins/bin/systems/configSystems.cfg");
-        std::cout << "Systems loaded!" << std::endl;
+        engine.loadSystems("./plugins/bin/systems/", "./plugins/bin/systems/configSystems.cfg");
         engine.registerComponent<Components::Position>(positionPluginPath);
-        std::cout << "Component loaded!" << std::endl;
 
         std::unique_ptr<Components::Position> position = engine.newComponent<Components::Position>(10, 20);
-        std::cout << "Position loaded: " << position->x << ", " << position->y << std::endl;
         engine.addComponent<Components::Position>(entity, std::move(position));
 
-        // SparseArray<Components::Position> &positionComponents = reg.componentManager().getComponents<Components::Position>();
-        // for (std::unique_ptr<Components::Position> &pos : positionComponents) {
-        //     if (!pos)
-        //         continue;
-        //     std::cout << "Position: " << pos->x << ", " << pos->y << std::endl;
-        // }
+        SparseArray<Components::Position> &positionComponents = reg.componentManager().getComponents<Components::Position>();
+        std::cout << "Position components: " << std::endl;
+        for (std::unique_ptr<Components::Position> &pos : positionComponents) {
+            if (!pos)
+                continue;
+            std::cout << "Position: " << pos->x << ", " << pos->y << std::endl;
+        }
     } catch (std::runtime_error &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 84;
