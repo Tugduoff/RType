b9970011b57b4e1f2dc4787cbc7f3803fc634ac7
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-29 17:31:51 +0200

    :recycle: Refactored the code to suit last changes on Game engine class.

--- Main.cpp
+++ Main.cpp
@@ -5,20 +5,25 @@
 ** Main.cpp file
 */
 
+#include "GameEngine.hpp"
 #include "ECS/registry/Registry.hpp"
 #include "ECS/utilities/SparseArray.hpp"
 #include "plugins/components/IComponent.hpp"
 #include "plugins/components/position/Position.hpp"
 
 int main() {
-    ECS::Registry reg;
+    Engine::GameEngine engine;
     std::string positionPluginPath = "./plugins/bin/components/Position.so";
+    std::string configSystemPath = "./plugins/bin/systems/Config.so";
 
     try {
-        reg.componentManager().registerComponent<Components::Position>(positionPluginPath);
+        ECS::Registry &reg = engine.getRegistry();
         ECS::Entity entity = reg.entityManager().spawnEntity();
-        std::unique_ptr<Components::Position> posComponent = reg.componentManager().loadComponent<Components::Position>(10, 20);
-        reg.componentManager().addComponent<Components::Position>(entity, std::move(posComponent));
+
+        engine.loadSystems("./plugins/bin/systems/", "./plugins/bin/systems/configSystems.cfg");
+        engine.registerComponent<Components::Position>(positionPluginPath);
+
+        reg.componentManager().addComponent<Components::Position>(entity, std::move(engine.newComponent<Components::Position>(10, 20)));
 
         SparseArray<Components::Position> &positionComponents = reg.componentManager().getComponents<Components::Position>();
         for (std::unique_ptr<Components::Position> &pos : positionComponents) {
