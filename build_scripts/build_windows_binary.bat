4d4cb8cd3758592915b7856df5075e83b47f2641
Clément Baziret <clement.baziret@epitech.eu>
2024-09-30 13:41:33 +0200

    :heavy_minus_sign: Removed useless commented code in windows batch script

--- build_scripts/build_windows_binary.bat
+++ build_scripts/build_windows_binary.bat
@@ -1,19 +1,6 @@
-@REM setlocal enabledelayedexpansion
-
-REM Define directories
 set BUILD_DIR=.\build
 set BINARY_DIR=.\bin
 
-@REM REM Define SFML .dll files (Windows equivalent to .so)
-@REM set SFML_DLL_FILES=(
-@REM     "libsfml-audio-2.dll"
-@REM     "libsfml-graphics-2.dll"
-@REM     "libsfml-network-2.dll"
-@REM     "libsfml-window-2.dll"
-@REM     "libsfml-system-2.dll"
-@REM )
-
-REM Compile the R-Type project
 echo R-Type Compilation...
 
 if not exist %BUILD_DIR% (
@@ -32,12 +19,5 @@ if errorlevel 1 (
     exit 84
 )
 cd ..
-
-REM Find all SFML .dll files and copy them to the binary directory
-@REM for %%F in (%BUILD_DIR%\*.dll) do (
-@REM     @REM copy %%F %BINARY_DIR%
-@REM     echo "%%F"
-@REM )
-@REM echo All SFML .dll files have been copied to %BINARY_DIR%.
 
 echo Windows build completed.
