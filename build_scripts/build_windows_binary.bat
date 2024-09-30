37ad610dcbddf15a271b9946ca1f3a8eaa719be9
Clément Baziret <clement.baziret@epitech.eu>
2024-09-30 12:56:44 +0200

    :heavy_plus_sign: Added windows compilation script and Dockerfile

---
+++ build_scripts/build_windows_binary.bat
@@ -0,0 +1,60 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Define directories
+set BUILD_DIR=.\build
+set BINARY_DIR=.\bin
+
+REM Define SFML .dll files (Windows equivalent to .so)
+set SFML_DLL_FILES=(
+    "libsfml-audio-2.dll"
+    "libsfml-graphics-2.dll"
+    "libsfml-network-2.dll"
+    "libsfml-window-2.dll"
+    "libsfml-system-2.dll"
+)
+
+REM Define other dependencies
+set DLL_FILES=(
+    "libX11.dll"
+    "libXrandr.dll"
+    "libXcursor.dll"
+    "libXrender.dll"
+    "libGL.dll"
+    "libudev.dll"
+    "libfreetype.dll"
+    "libpng16.dll"
+    "libbrotlidec.dll"
+    "libbrotlicommon.dll"
+)
+
+REM Compile the R-Type project
+echo R-Type Compilation...
+
+if not exist %BUILD_DIR% (
+    mkdir %BUILD_DIR%
+)
+
+cd %BUILD_DIR%
+cmake .. 
+if errorlevel 1 (
+    echo Compilation failed
+    exit /b 84
+)
+cmake --build .
+if errorlevel 1 (
+    echo Compilation failed
+    exit /b 84
+)
+cd ..
+
+REM Find all SFML .dll files and copy them to the binary directory
+for %%F in (%BUILD_DIR%\*.dll) do (
+    @REM copy %%F %BINARY_DIR%
+    echo %%F
+)
+@REM echo All SFML .dll files have been copied to %BINARY_DIR%.
+
+echo Windows build completed.
+
+pause
