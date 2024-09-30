e26dea2f9f94c145c7fcee24c2bdfd76e9de5526
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-30 15:51:17 +0200

    :wrench: Fixed error in component plugins path.

--- plugins/systems/move/Move.cpp
+++ plugins/systems/move/Move.cpp
@@ -39,9 +39,9 @@ void Systems::MoveSystem::run(Engine::GameEngine &engine)
 
 void Systems::MoveSystem::init(Engine::GameEngine &engine)
 {
-    if (!engine.registerComponent<Components::Position>("/plugins/bin/components/Position.so"))
+    if (!engine.registerComponent<Components::Position>("./plugins/bin/components/Position.so"))
         std::cerr << "Error: Could not register Position component in system Move" << std::endl;
-    if (!engine.registerComponent<Components::Velocity>("/plugins/bin/components/Velocity.so"))
+    if (!engine.registerComponent<Components::Velocity>("./plugins/bin/components/Velocity.so"))
         std::cerr << "Error: Could not register Velocity component in system Move" << std::endl;
 }
 
