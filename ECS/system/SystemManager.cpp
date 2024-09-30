7cd49c8a206d927e99162b1737110a97ba084685
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-09-30 14:56:11 +0200

    :heavy_plus_sign: Added new function initSystems declaration and modified function run prototype.

--- ECS/system/SystemManager.cpp
+++ ECS/system/SystemManager.cpp
@@ -6,15 +6,23 @@
 */
 
 #include "SystemManager.hpp"
+#include "GameEngine.hpp"
 
 void ECS::SystemManager::addSystem(std::unique_ptr<Systems::ISystem> system)
 {
     __systems.push_back(std::move(system));
 }
 
-void ECS::SystemManager::run(Registry &reg)
+void ECS::SystemManager::run(Engine::GameEngine &engine)
 {
     for (auto &system : __systems) {
-        system->run(reg);
+        system->run(engine);
+    }
+}
+
+void ECS::SystemManager::initSystems(Engine::GameEngine &engine)
+{
+    for (auto &system : __systems) {
+        system->init(engine);
     }
 }
