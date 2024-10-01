4a0a0784e59ecc3c0f8bd1a12e8f3ab27bc6556e
Clément Baziret <clement.baziret@epitech.eu>
2024-10-01 13:13:01 +0200

    :green_heart: Added some debug to see what is going on

--- windows_build_rtype.bat
+++ windows_build_rtype.bat
@@ -4,13 +4,16 @@ if not exist %BUILD_DIR% (
     mkdir %BUILD_DIR%
 )
 
+echo R-Type Building...
 cd %BUILD_DIR%
 cmake .. 
 if errorlevel 1 (
     echo Compilation failed
     exit 84
 )
-cmake --build . --target all
+
+echo R-Type Compilation...
+cmake --build .
 if errorlevel 1 (
     echo Compilation failed
     exit 84
