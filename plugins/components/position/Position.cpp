cbbcbb03538754ef9124ad50403f3d14953b1f67
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-02 13:24:24 +0200

    :heavy_plus_sign: Added new entryId function declaration that returns the id string of the component.

--- plugins/components/position/Position.cpp
+++ plugins/components/position/Position.cpp
@@ -6,9 +6,16 @@
 */
 
 #include <memory>
+#include <iostream>
+#include <stdexcept>
 #include "Position.hpp"
 
 extern "C" std::unique_ptr<Components::IComponent> entryPoint(uint32_t x, uint32_t y)
 {
     return std::make_unique<Components::Position>(x, y);
+}
+
+extern "C" std::string entryID()
+{
+    return "Position";
 }
