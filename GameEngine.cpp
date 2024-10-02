ca089f054a9ec59d9c9d62a9b51e062d925e22b7
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-02 13:21:46 +0200

    :heavy_plus_sign: Added better support for config args in loadSystems.

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -8,7 +8,7 @@
 #include "GameEngine.hpp"
 #include <libconfig.h++>
 
-void Engine::GameEngine::loadSystems(const std::string &systemsFolderPath, const std::string &systemsConfigFile)
+void Engine::GameEngine::loadSystems(const std::string &systemsConfigFile)
 {
     libconfig::Config cfg;
 
@@ -17,13 +17,43 @@ void Engine::GameEngine::loadSystems(const std::string &systemsFolderPath, const
         libconfig::Setting &root = cfg.getRoot();
         if (root.exists("systems")) {
             libconfig::Setting &systems = root["systems"];
+            std::string systemsFolderPath = "./plugins/bin/systems/";
             for (int i = 0; i < systems.getLength(); ++i) {
-                std::string systemName = systems[i].c_str();
-                std::string systemPath = systemsFolderPath + systemName + ".so";
-                DLLoader loader(systemPath);
-                std::unique_ptr<Systems::ISystem> system = loader.getInstance<Systems::ISystem>("entryPoint", loader);
-                __registry.systemManager().addSystem(std::move(system));
-                __systemLoaders.push_back(std::move(loader));
+                libconfig::Setting &systemConfig = systems[i];
+                std::string systemPath = systemsFolderPath;
+                std::string dirName;
+                std::string systemName;
+
+                if (systemConfig.lookupValue("dir", dirName)) {
+                    systemPath += dirName;
+                }
+                if (systemConfig.lookupValue("name", systemName)) {
+                    systemPath += systemName + ".so";
+
+                    std::cout << "Loading system: " << systemPath << std::endl;
+                    std::vector<std::any> instanceArgs;
+                    if (systemConfig.exists("args")) {
+                        libconfig::Setting &args = systemConfig["args"];
+                        for (int k = 0; k < args.getLength(); ++k) {
+                            libconfig::Setting &arg = args[k];
+                            if (arg.getType() == libconfig::Setting::TypeInt) {
+                                instanceArgs.push_back(arg.operator int());
+                            } else if (arg.getType() == libconfig::Setting::TypeFloat) {
+                                instanceArgs.push_back(arg.operator double());
+                            } else if (arg.getType() == libconfig::Setting::TypeString) {
+                                instanceArgs.push_back(arg.c_str());
+                            } else if (arg.getType() == libconfig::Setting::TypeBoolean) {
+                                instanceArgs.push_back(arg.operator bool());
+                            } else {
+                                std::cerr << "Unsupported argument type" << std::endl;
+                            }
+                        }
+                    }
+                    DLLoader loader(systemPath);
+                    std::unique_ptr<Systems::ISystem> system = loader.getInstance<Systems::ISystem>("entryPoint", instanceArgs);
+                    __registry.systemManager().addSystem(std::move(system));
+                    __systemLoaders.push_back(std::move(loader));
+                }
             }
             initSystems();
         }
