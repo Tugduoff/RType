3fd14d0f6d726ca009f9b00afa462740aa29bb52
tibaud le maout <tibaud.le-maout@epitech.eu>
2024-10-02 10:08:31 +0200

    :wrench: Fixed arguments demanded for entrypoints not matching its definition

--- plugins/components/collider/Collider.cpp
+++ plugins/components/collider/Collider.cpp
@@ -8,7 +8,13 @@
 #include <memory>
 #include "Collider.hpp"
 
-extern "C" std::unique_ptr<Components::IComponent> entryPoint(uint8_t shape, uint32_t radius, uint32_t width, uint32_t height)
+extern "C" std::unique_ptr<Components::IComponent> entryPoint(uint32_t shape, uint32_t radius, uint32_t width, uint32_t height)
 {
-    return std::make_unique<Components::Collider>(shape, radius, width, height);
+    if (shape == 0) {
+        return std::make_unique<Components::Collider>(static_cast<float>(radius));
+    } else if (shape == 1) {
+        return std::make_unique<Components::Collider>(static_cast<float>(width), static_cast<float>(height));
+    }
+    throw std::invalid_argument("Invalid shape type provided");
 }
+
