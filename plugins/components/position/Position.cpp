05543fa70834cab49b9c9ca8770eb44f4fda6c28
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-02 21:22:37 +0200

    :heavy_plus_sign: Added a new ctor that takes a libconfig::Setting as parameter and added a new entryPoint functino named entryConfig.

--- plugins/components/position/Position.cpp
+++ plugins/components/position/Position.cpp
@@ -10,12 +10,24 @@
 #include <stdexcept>
 #include "Position.hpp"
 
-extern "C" std::unique_ptr<Components::IComponent> entryPoint(uint32_t x, uint32_t y)
+extern "C" std::unique_ptr<Components::IComponent> entryPoint(uint32_t x, uint32_t y, uint32_t layer)
 {
-    return std::make_unique<Components::Position>(x, y);
+    return std::make_unique<Components::Position>(x, y, layer);
+}
+
+extern "C" std::unique_ptr<Components::IComponent> entryConfig(libconfig::Setting &config)
+{
+    return std::make_unique<Components::Position>(config);
 }
 
 extern "C" std::string entryID()
 {
     return "Position";
+}
+
+Components::Position::Position(libconfig::Setting &config)
+{
+    config.lookupValue("x", x);
+    config.lookupValue("y", y);
+    config.lookupValue("layer", layer);
 }
