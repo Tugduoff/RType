95bad916cbe2d943672f58709beafd436fc1877b
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-05 15:55:10 +0200

    :recycle: Removed `DLLoader::getInstance2`

--- src/DLLoader/DLLoader.hpp
+++ src/DLLoader/DLLoader.hpp
@@ -85,28 +85,7 @@ class DLLoader {
         */
         template<typename T, typename... Args>
         std::unique_ptr<T> getUniqueInstance(const std::string &entryPointName = "entryPoint", Args&&... args) {
-            return std::unique_ptr<T>(callFunction<T *>(entryPointName, args...));
-        };
-
-        /**
-        * @brief Retrieves a function pointer from the library and calls it to create and return a new instance of type T.
-        *
-        * @tparam T The type of the object to create.
-        * @tparam Args The types of the arguments to pass to the function.
-        *
-        * @param entryPointName The name of the function to retrieve from the library (default: "entryPoint").
-        * @param args The arguments to pass to the function.
-        *
-        * @return A pointer to a new instance of type T.
-        *
-        * @throw DLLExceptions If the function pointer cannot be retrieved.
-        */
-        template<typename T>
-        std::unique_ptr<T> getInstance2(const std::string &entryPointName, libconfig::Setting &config) {
-            using EntryPointFunc = T *(*)(libconfig::Setting &);
-            EntryPointFunc entryPoint = getSymbolAddress<EntryPointFunc>(entryPointName);
-
-            return std::unique_ptr<T>(entryPoint(config));
+            return std::unique_ptr<T>(callFunction<T *, Args...>(entryPointName, args...));
         };
 
         std::string getStringId(const std::string &entryPointName = "entryID") {
