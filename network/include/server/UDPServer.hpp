f57c9bfae8c94c379303bd65484adb3307e6cda1
Thomas <thomas.cluseau@epitech.eu>
2024-10-05 11:09:12 +0200

    :heavy_plus_sign: Server gets the endpoint from each client and sends them the list of component names if it is their first connection

--- network/include/server/UDPServer.hpp
+++ network/include/server/UDPServer.hpp
@@ -4,6 +4,7 @@
 ** File description:
 ** udp server
 */
+#include <algorithm>
 #include "../IUDP.hpp"
 
 using boost::asio::ip::udp;
@@ -16,6 +17,10 @@ class UDPServer : public IUDP {
         UDPServer(boost::asio::io_context& io_context, short port)
             : socket_(io_context, udp::endpoint(udp::v4(), port)) {
             std::cout << "Server started on port " << port << std::endl;
+            /* HARD CODE */
+            size_max = 10;
+            components_names = {"Position", "Velocity", "Health"};
+            /* --------- */
             start_receive();
         }
 
@@ -23,15 +28,41 @@ class UDPServer : public IUDP {
         udp::socket socket_;
         udp::endpoint remote_endpoint_;
         std::array<char, 1024> recv_buffer_;
+        std::vector<udp::endpoint> client_endpoints;
+        std::size_t size_max;
+        std::vector<std::string> components_names;
 
         void start_receive() override {
-            socket_.async_receive_from( boost::asio::buffer(recv_buffer_), remote_endpoint_,
+            socket_.async_receive_from(boost::asio::buffer(recv_buffer_), remote_endpoint_,
                 [this](boost::system::error_code ec, std::size_t bytes_recvd) {
-                    if (!ec && bytes_recvd > 0)
-                        std::cout << "Received: " << std::string(recv_buffer_.data(), bytes_recvd) << std::endl;
-                    
+                    if (!ec && bytes_recvd > 0) {
+                        std::cout << "Client connected: " << remote_endpoint_ << std::endl;
+
+                        // if first connection
+                        if (std::find(client_endpoints.begin(), client_endpoints.end(), remote_endpoint_) == client_endpoints.end()) {
+                            client_endpoints.push_back(remote_endpoint_);
+                            std::cout << "New client added: " << remote_endpoint_ << std::endl;
+                            send_components_names();
+                        }
+
+                        std::string message(recv_buffer_.data(), bytes_recvd);
+                        std::cout << "Received message from client (" << remote_endpoint_ << "): " << message << std::endl;
+                    }
                     start_receive();
                 });
+        }
+
+        void send_components_names() {
+            for (const auto& message : components_names) {
+                socket_.async_send_to(
+                    boost::asio::buffer(message), remote_endpoint_,
+                    [this, message](boost::system::error_code ec, std::size_t size_max) {
+                        if (!ec) {
+                            std::cout << "Message sent to client: " << message << std::endl;
+                        }
+                    }
+                );
+            }
         }
 };
 
