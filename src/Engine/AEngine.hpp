df9e3d03170e5586ccf4d1802cbe95e20aff0d0b
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-07 15:47:22 +0200

    :heavy_plus_sign: Added an abstract class of the IEngine interface

---
+++ src/Engine/AEngine.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** AEngine.hpp file
+*/
+
+#ifndef AENGINE_HPP
+    #define AENGINE_HPP
+
+    #include "IEngine.hpp"
+
+namespace Engine {
+    class AEngine : public IEngine {
+        public:
+
+            AEngine() = default;
+            ~AEngine() = default;
+
+        private:
+    };
+};
+
+#endif // AENGINE_HPP
