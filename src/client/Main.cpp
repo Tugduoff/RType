47132a11257e4bbf7d17cd6042c6d4e93c53404e
Clément Baziret <clement.baziret@epitech.eu>
2024-10-08 22:58:43 +0200

    :heavy_plus_sign: Fully added network to existing client

--- src/client/Main.cpp
+++ src/client/Main.cpp
@@ -1,9 +1,23 @@
 #include <SFML/Graphics.hpp>
 #include "Window.hpp"
+#include "Network/RTypeClient.hpp"
 
 int main()
 {
     Window win(1920, 1080, "test");
+
+    RTypeClient conn("127.0.0.1", "8080");
+
+    std::string message = "Hello from client!";
+
+    conn.send(message);
+    std::cout << "Sent message: " << UDPConnection::binaryToStr(UDPConnection::strToBinary(message)) << std::endl;
+
+    std::unordered_map<uint8_t, std::string> compNames = conn.initGame();
+    for (const auto &name : compNames) {
+        std::cout << "Commponent n°" << name.first << ": " << name.second << std::endl;
+    }
+
     while (win.isOpen()) {
         win.clear(sf::Color::Blue);
         win.display();
