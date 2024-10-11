2ec2b7f8c174899c850089441fa9ab9b87edb4b8
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-11 18:56:42 +0200

    :heavy_plus_sign: Added catching for almost every key we want to catch in the Input class of the sfml system.

--- plugins/systems/sfml/Input.cpp
+++ plugins/systems/sfml/Input.cpp
@@ -27,11 +27,51 @@ void Systems::Input::run(Engine::GameEngine &engine, sf::RenderWindow &window)
 {
     sf::Event event;
 
-    std::cout << "Running Input" << std::endl;
-    window.pollEvent(event);
-    if (event.type == sf::Event::Closed) {
-        std::cerr << "Window closed" << std::endl;
-        window.close();
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed) {
+            std::cerr << "Window closed" << std::endl;
+            window.close();
+        }
+        if (event.type == sf::Event::KeyPressed) {
+            if (event.key.code == sf::Keyboard::Escape) {
+                std::cerr << "Escape pressed" << std::endl;
+                window.close();
+            }
+        }
+        handleInput(engine, event);
     }
-    std::cout << "Input done" << std::endl;
+}
+
+void Systems::Input::handleInput(Engine::GameEngine &engine, sf::Event &event)
+{
+    if (event.type == sf::Event::KeyPressed) {
+        if (event.key.code == sf::Keyboard::Z) {
+            std::cerr << "Z key pressed" << std::endl;
+        }
+        if (event.key.code == sf::Keyboard::Q) {
+            std::cerr << "Q key pressed" << std::endl;
+        }
+        if (event.key.code == sf::Keyboard::S) {
+            std::cerr << "S key pressed" << std::endl;
+        }
+        if (event.key.code == sf::Keyboard::D) {
+            std::cerr << "D key pressed" << std::endl;
+        }
+    }
+    if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            std::cerr << "Left mouse button pressed" << std::endl;
+        }
+        if (event.mouseButton.button == sf::Mouse::Right) {
+            std::cerr << "Right mouse button pressed" << std::endl;
+        }
+    }
+    if (event.type == sf::Event::MouseButtonReleased) {
+        if (event.mouseButton.button == sf::Mouse::Left) {
+            std::cerr << "Left mouse button released" << std::endl;
+        }
+        if (event.mouseButton.button == sf::Mouse::Right) {
+            std::cerr << "Right mouse button released" << std::endl;
+        }
+    }
 }
