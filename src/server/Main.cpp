003c33323d6a535fb1fb85544ddb4eb772173944
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-12 15:37:55 +0200

    :alembic: Added example for `IComponent::any_cast` in temporary main

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -13,6 +13,8 @@
 #include "plugins/components/velocity/Velocity.hpp"
 #include <exception>
 #include <iostream>
+#include <typeindex>
+#include <vector>
 
 void displayComponents(ECS::Registry &reg)
 {
@@ -35,6 +37,30 @@ void displayComponents(ECS::Registry &reg)
     }
 }
 
+template<typename It>
+void displayPolymorphic(Engine::GameEngine &engine, It begin, It end)
+{
+    for (auto it = begin; it != end; ++it) {
+        std::type_index &idx = *it;
+
+        std::cout << "For type index {" << idx.name() << "}:" << std::endl;
+
+        auto &dummyComp = engine.getComponentFromId(idx);
+
+        auto &sparseArray = dummyComp->any_cast(
+            engine.getRegistry().componentManager().getComponents(idx)
+        );
+
+        for (auto const &comp : sparseArray) {
+            std::cout << "    " << comp->getId() << ": {";
+            for (const auto &byte : comp->serialize()) {
+                std::cout << (int)byte << ", ";
+            }
+            std::cout << "}" << std::endl;
+        }
+    }
+}
+
 void updateComponent(size_t id, std::string name, std::vector<uint8_t> data)
 {
     std::cout << "Updating component: " << name << " with ID: " << id << std::endl;
@@ -47,6 +73,8 @@ int main() {
     ECS::Registry &reg = engine.getRegistry();
     ECS::Entity entity = reg.entityManager().spawnEntity();
 
+    std::vector<std::type_index> types = {typeid(Components::Velocity), typeid(Components::Position)};
+
     try {
         engine.loadSystems("./plugins/bin/systems/configSystems.cfg");
 
@@ -57,6 +85,8 @@ int main() {
         reg.componentManager().addComponent<Components::Velocity>(entity, std::move(velocity));
 
         displayComponents(reg);
+
+        displayPolymorphic(engine, types.begin(), types.end());
 
         engine.runSystems();
         engine.runSystems();
