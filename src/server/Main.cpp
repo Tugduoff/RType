5d4cf213ffac690be6e5991821ac2e669220b231
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-12 23:03:56 +0200

    :heavy_plus_sign: Added new components types in main for better showcase of whats possible.

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -12,31 +12,13 @@
 #include "plugins/components/position/Position.hpp"
 #include "plugins/components/velocity/Velocity.hpp"
 #include "plugins/components/controllable/Controllable.hpp"
+#include "plugins/components/visible/Visible.hpp"
+#include "plugins/components/health/Health.hpp"
+#include "plugins/components/collider/Collider.hpp"
 #include <exception>
 #include <iostream>
 #include <typeindex>
 #include <vector>
-
-void displayComponents(ECS::Registry &reg)
-{
-    SparseArray<Components::Position> &positionComponents = reg.componentManager().getComponents<Components::Position>();
-    SparseArray<Components::Velocity> &velocityComponents = reg.componentManager().getComponents<Components::Velocity>();
-
-    std::cout << "Displaying components: " << std::endl;
-    std::cout << "\nPosition components: \n" << std::endl;
-    for (std::unique_ptr<Components::Position> &pos : positionComponents) {
-        if (!pos)
-            continue;
-        std::cout << "Position: " << pos->x << ", " << pos->y << std::endl;
-    }
-
-    std::cout << "\nVelocity components: \n" << std::endl;
-    for (std::unique_ptr<Components::Velocity> &vel : velocityComponents) {
-        if (!vel)
-            continue;
-        std::cout << "Velocity: " << vel->x << ", " << vel->y << std::endl;
-    }
-}
 
 template<typename It>
 void displayPolymorphic(Engine::GameEngine &engine, It begin, It end)
@@ -46,7 +28,7 @@ void displayPolymorphic(Engine::GameEngine &engine, It begin, It end)
     for (auto it = begin; it != end; ++it) {
         std::type_index &idx = *it;
 
-        std::cout << "For type index {" << idx.name() << "}:" << std::endl;
+        std::cout << "For type index {" << idx.name() << "}:\n" << std::endl;
 
         auto &dummyComp = engine.getComponentFromId(idx);
 
@@ -60,19 +42,27 @@ void displayPolymorphic(Engine::GameEngine &engine, It begin, It end)
             for (const auto &byte : comp->serialize()) {
                 if (i != 0)
                     std::cout << ", ";
-                std::cout << (int)byte;
+                std::cout << (unsigned)byte;
                 i++;
             }
             std::cout << "}" << std::endl;
         }
+        std::cout << std::endl;
     }
 }
 
 void updateComponent(size_t id, std::string name, std::vector<uint8_t> data)
 {
     std::cout << "Updating component: " << name << " with ID: " << id << std::endl;
-    for (auto u : data)
-        std::cout << u << " ";
+    std::cout << "    {";
+    int i = 0;
+    for (const auto &byte : data) {
+        if (i != 0)
+            std::cout << ", ";
+        std::cout << (unsigned)byte;
+        i++;
+    }
+    std::cout << "}" << std::endl;
 }
 
 int main() {
@@ -83,11 +73,17 @@ int main() {
     std::vector<std::type_index> types = {
         typeid(Components::Velocity),
         typeid(Components::Position),
-        typeid(Components::Controllable)
+        typeid(Components::Controllable),
+        typeid(Components::Visible),
+        typeid(Components::Health),
+        typeid(Components::Collider)
     };
 
     try {
         engine.registerComponent<Components::Controllable>("./plugins/bin/components/", "Controllable");
+        engine.registerComponent<Components::Visible>("./plugins/bin/components/", "Visible");
+        engine.registerComponent<Components::Health>("./plugins/bin/components/", "Health");
+        engine.registerComponent<Components::Collider>("./plugins/bin/components/", "Collider");
         engine.loadSystems("./plugins/bin/systems/configSystems.cfg");
 
         std::unique_ptr<Components::Position> position = engine.newComponent<Components::Position>(10, 20, 1);
@@ -95,8 +91,6 @@ int main() {
 
         reg.componentManager().addComponent<Components::Position>(entity, std::move(position));
         reg.componentManager().addComponent<Components::Velocity>(entity, std::move(velocity));
-
-        displayComponents(reg);
 
         displayPolymorphic(engine, types.begin(), types.end());
 
