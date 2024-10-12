1cd65004fd6e866be67a4c69e5eaa14239b10dab
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-13 00:08:59 +0200

    :sparkles: Added event hooks for entity creation/deletion

---
+++ src/ECS/registry/Registry.cpp
@@ -0,0 +1,21 @@
+#include "Registry.hpp"
+#include "ECS/entity/Entity.hpp"
+
+ECS::Entity ECS::Registry::createEntity()
+{
+    Entity entity = this->__entityManager.spawnEntity();
+
+    for (auto const &cb : this->__createCallbacks) {
+        cb(entity);
+    }
+    return entity;
+}
+
+void ECS::Registry::killEntity(const ECS::Entity &entity)
+{
+    this->__componentManager.removeAllFromEntity(entity);
+    this->__entityManager.killEntity(entity);
+    for (auto const &cb : this->__killCallbacks) {
+        cb(entity);
+    }
+}
