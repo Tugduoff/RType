56dd3a1961b430850041337d7a93267d85fa1fd4
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-13 02:29:48 +0200

    :heavy_plus_sign: Added new things in the main to showcase new updates.

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -15,6 +15,7 @@
 #include "plugins/components/visible/Visible.hpp"
 #include "plugins/components/health/Health.hpp"
 #include "plugins/components/collider/Collider.hpp"
+#include "plugins/components/acceleration/Acceleration.hpp"
 #include <exception>
 #include <iostream>
 #include <typeindex>
@@ -37,6 +38,8 @@ void displayPolymorphic(Engine::GameEngine &engine, It begin, It end)
         );
 
         for (auto const &comp : sparseArray) {
+            if (!comp)
+                continue;
             std::cout << "    " << comp->getId() << ": {";
             i = 0;
             for (const auto &byte : comp->serialize()) {
@@ -76,25 +79,37 @@ int main() {
         typeid(Components::Controllable),
         typeid(Components::Visible),
         typeid(Components::Health),
-        typeid(Components::Collider)
+        typeid(Components::Collider),
+        typeid(Components::Acceleration)
     };
 
     try {
-        engine.registerComponent<Components::Controllable>("./plugins/bin/components/", "Controllable");
         engine.registerComponent<Components::Visible>("./plugins/bin/components/", "Visible");
         engine.registerComponent<Components::Health>("./plugins/bin/components/", "Health");
         engine.registerComponent<Components::Collider>("./plugins/bin/components/", "Collider");
+
         engine.loadSystems("./plugins/bin/systems/configSystems.cfg");
 
-        std::unique_ptr<Components::Position> position = engine.newComponent<Components::Position>(10, 20, 1);
-        std::unique_ptr<Components::Velocity> velocity = engine.newComponent<Components::Velocity>(2, 1);
+        std::unique_ptr<Components::Controllable> ctrl = engine.newComponent<Components::Controllable>();
+        std::unique_ptr<Components::Velocity> vel = engine.newComponent<Components::Velocity>(0, 0, (uint8_t)90);
+        std::unique_ptr<Components::Acceleration> accel = engine.newComponent<Components::Acceleration>(5, -5, 5, -5);
+        std::unique_ptr<Components::Position> pos = engine.newComponent<Components::Position>(10, 20, 1);
 
-        reg.componentManager().addComponent<Components::Position>(entity, std::move(position));
-        reg.componentManager().addComponent<Components::Velocity>(entity, std::move(velocity));
+        reg.componentManager().addComponent<Components::Controllable>(entity, std::move(ctrl));
+        reg.componentManager().addComponent<Components::Velocity>(entity, std::move(vel));
+        reg.componentManager().addComponent<Components::Acceleration>(entity, std::move(accel));
+        reg.componentManager().addComponent<Components::Position>(entity, std::move(pos));
 
         displayPolymorphic(engine, types.begin(), types.end());
 
+        std::cout << "#######################################" << std::endl;
+
+        engine.getRegistry().componentManager().getComponents<Components::Controllable>()[0]->inputs[(int)Action::FORWARD] = true;
+
         engine.runSystems();
+
+        displayPolymorphic(engine, types.begin(), types.end()); // This crashes the program
+
     } catch (std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 84;
