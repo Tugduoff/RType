d0ebba4f19d4e8ef094ad28ae1982c5f3f8e5892
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-13 15:39:17 +0200

    :wrench: Modified main to showcase new possibilities.

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -8,6 +8,7 @@
 #include "GameEngine/GameEngine.hpp"
 #include "ECS/registry/Registry.hpp"
 #include "ECS/utilities/SparseArray.hpp"
+#include "Chrono.hpp"
 
 #include "plugins/components/IComponent.hpp"
 #include "plugins/components/position/Position.hpp"
@@ -75,8 +76,9 @@ void updateComponent(size_t id, std::string name, std::vector<uint8_t> data)
 
 int main() {
     Engine::GameEngine engine(updateComponent);
-    ECS::Registry &reg = engine.getRegistry();
-    ECS::Entity entity = reg.entityManager().spawnEntity();
+    // ECS::Registry &reg = engine.getRegistry();
+    // ECS::Entity entity = reg.entityManager().spawnEntity();
+    Chrono chrono;
 
     std::vector<std::type_index> types = {
         typeid(Components::Velocity),
@@ -95,52 +97,51 @@ int main() {
 
         engine.loadSystems("./plugins/bin/systems/configSystems.cfg");
 
-        while (true) {
-            engine.runSystems();
-        }
-        std::unordered_map<enum Action, enum Key> keyBindings = {
-            {Action::FORWARD, Key::Z},
-            {Action::BACKWARD, Key::S},
-            {Action::LEFT, Key::Q},
-            {Action::RIGHT, Key::D},
-            {Action::ACTION1, Key::LEFT_CLICK},
-            {Action::ACTION2, Key::RIGHT_CLICK},
-            {Action::ACTION3, Key::MIDDLE_CLICK},
-            {Action::ACTION4, Key::NUM_0},
-            {Action::ACTION5, Key::NUM_1},
-            {Action::ACTION6, Key::NUM_2},
-            {Action::ACTION7, Key::NUM_3},
-            {Action::ACTION8, Key::NUM_4},
-            {Action::ACTION9, Key::NUM_5},
-            {Action::ACTION10, Key::NUM_6}
-        };
-        std::unique_ptr<Components::SpriteIDComponent> spriteId = engine.newComponent<Components::SpriteIDComponent>(Components::SpriteID::Player);
-        std::unique_ptr<Components::SpriteComponent> sprite = std::make_unique<Components::SpriteComponent>();
-        std::unique_ptr<Components::Controllable> ctrl = engine.newComponent<Components::Controllable>(keyBindings);
-        std::unique_ptr<Components::Velocity> vel = engine.newComponent<Components::Velocity>(0, 0, (uint8_t)90);
-        std::unique_ptr<Components::Acceleration> accel = engine.newComponent<Components::Acceleration>(5, -5, 5, -5);
-        std::unique_ptr<Components::Position> pos = engine.newComponent<Components::Position>(10, 20, 1);
+        // std::map<enum Action, enum Key> keyBindings = {
+        //     {Action::FORWARD, Key::D},
+        //     {Action::BACKWARD, Key::Q},
+        //     {Action::RIGHT, Key::S},
+        //     {Action::LEFT, Key::Z},
+        //     {Action::ACTION1, Key::LEFT_CLICK},
+        //     {Action::ACTION2, Key::RIGHT_CLICK},
+        //     {Action::ACTION3, Key::MIDDLE_CLICK},
+        //     {Action::ACTION4, Key::NUM_0},
+        //     {Action::ACTION5, Key::NUM_1},
+        //     {Action::ACTION6, Key::NUM_2},
+        //     {Action::ACTION7, Key::NUM_3},
+        //     {Action::ACTION8, Key::NUM_4},
+        //     {Action::ACTION9, Key::NUM_5},
+        //     {Action::ACTION10, Key::NUM_6}
+        // };
+        // std::unique_ptr<Components::SpriteIDComponent> spriteId = engine.newComponent<Components::SpriteIDComponent>(Components::SpriteID::Player);
+        // std::unique_ptr<Components::SpriteComponent> sprite = std::make_unique<Components::SpriteComponent>();
+        // std::unique_ptr<Components::Controllable> ctrl = engine.newComponent<Components::Controllable>(keyBindings);
+        // std::unique_ptr<Components::Velocity> vel = engine.newComponent<Components::Velocity>(0, 0, (uint8_t)90);
+        // std::unique_ptr<Components::Acceleration> accel = engine.newComponent<Components::Acceleration>(1, -1, 1, -1);
+        // std::unique_ptr<Components::Position> pos = engine.newComponent<Components::Position>(10, 20, 1);
 
-        reg.componentManager().addComponent<Components::SpriteIDComponent>(entity, std::move(spriteId));
-        reg.componentManager().addComponent<Components::SpriteComponent>(entity, std::move(sprite));
-        reg.componentManager().addComponent<Components::Controllable>(entity, std::move(ctrl));
-        reg.componentManager().addComponent<Components::Velocity>(entity, std::move(vel));
-        reg.componentManager().addComponent<Components::Acceleration>(entity, std::move(accel));
-        reg.componentManager().addComponent<Components::Position>(entity, std::move(pos));
+        // reg.componentManager().addComponent<Components::SpriteIDComponent>(entity, std::move(spriteId));
+        // reg.componentManager().addComponent<Components::SpriteComponent>(entity, std::move(sprite));
+        // reg.componentManager().addComponent<Components::Controllable>(entity, std::move(ctrl));
+        // reg.componentManager().addComponent<Components::Velocity>(entity, std::move(vel));
+        // reg.componentManager().addComponent<Components::Acceleration>(entity, std::move(accel));
+        // reg.componentManager().addComponent<Components::Position>(entity, std::move(pos));
 
-        engine.getRegistry().componentManager().getComponents<Components::Controllable>()[0]->inputs[(int)Action::FORWARD] = true;
+        // engine.getRegistry().componentManager().getComponents<Components::Controllable>()[0]->inputs[(int)Action::FORWARD] = true;
 
         displayPolymorphic(engine, types.begin(), types.end());
 
         std::cout << "####################################### iteration 0\n" << std::endl;
 
-        std::string input;
         unsigned int i = 1;
-        while (std::getline(std::cin, input)) {
+        while (true) {
+            if (chrono.getElapsedTime() < 17)
+                continue;
             engine.runSystems();
             displayPolymorphic(engine, types.begin(), types.end());
             std::cout << "####################################### iteration: " << i++ << "\n" << std::endl;
-        }
+            chrono.restart();
+       }
 
     } catch (std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
