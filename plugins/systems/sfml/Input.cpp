d91d5f475135550a77de4ea0f77ef9457fc7e1c8
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-13 15:37:00 +0200

    :heavy_plus_sign: Added init of the controllable component in the init function of the input system. Added handlers for all keybinds.

--- plugins/systems/sfml/Input.cpp
+++ plugins/systems/sfml/Input.cpp
@@ -18,9 +18,10 @@ Systems::Input::Input()
 {
 }
 
-void Systems::Input::init(Engine::GameEngine &engine, sf::RenderWindow &window)
+void Systems::Input::init(Engine::GameEngine &engine)
 {
-    // Maybe register a controllable component here
+    if (!engine.registerComponent<Components::Controllable>("./plugins/bin/components/", "Controllable"))
+        std::cerr << "Error: Could not register Controllable component in system Input" << std::endl;
 }
 
 void Systems::Input::run(Engine::GameEngine &engine, sf::RenderWindow &window)
@@ -39,39 +40,92 @@ void Systems::Input::run(Engine::GameEngine &engine, sf::RenderWindow &window)
             }
         }
         handleInput(engine, event);
+        std::cout << std::endl;
     }
 }
 
 void Systems::Input::handleInput(Engine::GameEngine &engine, sf::Event &event)
 {
     if (event.type == sf::Event::KeyPressed) {
-        if (event.key.code == sf::Keyboard::Z) {
-            std::cerr << "Z key pressed" << std::endl;
+        for (auto &ctrl : engine.getRegistry().componentManager().getComponents<Components::Controllable>()) {
+            Key key = Key::UNKNOWN;
+            try {
+                key = __sfmlToKey.at(event.key.code);
+            } catch (std::exception &e) {
+                std::cerr << "Key not handled" << std::endl;
+            }
+            int index = 0;
+            for (auto &[action, actionKey] : ctrl->keyBindings) {
+                if (actionKey == key) {
+                    handleInputPressed(ctrl->inputs, ctrl->actions, index);
+                    return;
+                }
+                index++;
+            }
         }
-        if (event.key.code == sf::Keyboard::Q) {
-            std::cerr << "Q key pressed" << std::endl;
-        }
-        if (event.key.code == sf::Keyboard::S) {
-            std::cerr << "S key pressed" << std::endl;
-        }
-        if (event.key.code == sf::Keyboard::D) {
-            std::cerr << "D key pressed" << std::endl;
+    }
+    if (event.type == sf::Event::KeyReleased) {
+        for (auto &ctrl : engine.getRegistry().componentManager().getComponents<Components::Controllable>()) {
+            Key key = Key::UNKNOWN;
+            try {
+                key = __sfmlToKey.at(event.key.code);
+            } catch (std::exception &e) {
+                std::cerr << "Key not handled" << std::endl;
+            }
+            int index = 0;
+            for (auto &[action, actionKey] : ctrl->keyBindings) {
+                if (actionKey == key) {
+                    handleInputReleased(ctrl->inputs, ctrl->actions, index);
+                    return;
+                }
+                index++;
+            }
         }
     }
     if (event.type == sf::Event::MouseButtonPressed) {
-        if (event.mouseButton.button == sf::Mouse::Left) {
-            std::cerr << "Left mouse button pressed" << std::endl;
-        }
-        if (event.mouseButton.button == sf::Mouse::Right) {
-            std::cerr << "Right mouse button pressed" << std::endl;
+        for (auto &ctrl : engine.getRegistry().componentManager().getComponents<Components::Controllable>()) {
+            int index = 0;
+            for (auto &[action, actionKey] : ctrl->keyBindings) {
+                if ((actionKey == Key::LEFT_CLICK && event.mouseButton.button == sf::Mouse::Left) ||
+                    (actionKey == Key::RIGHT_CLICK && event.mouseButton.button == sf::Mouse::Right) ||
+                    (actionKey == Key::MIDDLE_CLICK && event.mouseButton.button == sf::Mouse::Middle)) {
+                        handleInputPressed(ctrl->inputs, ctrl->actions, index);
+                        return;
+                }
+                index++;
+            }
         }
     }
     if (event.type == sf::Event::MouseButtonReleased) {
-        if (event.mouseButton.button == sf::Mouse::Left) {
-            std::cerr << "Left mouse button released" << std::endl;
+        for (auto &ctrl : engine.getRegistry().componentManager().getComponents<Components::Controllable>()) {
+            int index = 0;
+            for (auto &[action, actionKey] : ctrl->keyBindings) {
+                if ((actionKey == Key::LEFT_CLICK && event.mouseButton.button == sf::Mouse::Left) ||
+                    (actionKey == Key::RIGHT_CLICK && event.mouseButton.button == sf::Mouse::Right) ||
+                    (actionKey == Key::MIDDLE_CLICK && event.mouseButton.button == sf::Mouse::Middle)) {
+                        handleInputReleased(ctrl->inputs, ctrl->actions, index);
+                        return;
+                }
+                index++;
+            }
         }
-        if (event.mouseButton.button == sf::Mouse::Right) {
-            std::cerr << "Right mouse button released" << std::endl;
-        }
+    }
+}
+
+void Systems::Input::handleInputPressed(std::array<bool, 4> &inputs, std::array<bool, 10> &actions, int index)
+{
+    if (index < 4) {
+        inputs[index] = true;
+    } else {
+        actions[index - 4] = true;
+    }
+}
+
+void Systems::Input::handleInputReleased(std::array<bool, 4> &inputs, std::array<bool, 10> &actions, int index)
+{
+    if (index < 4) {
+        inputs[index] = false;
+    } else {
+        actions[index - 4] = false;
     }
 }
