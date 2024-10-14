f0357e07bc4b0c3b077e54357689e29f25cb8531
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-14 14:56:00 +0200

    :heavy_plus_sign: Added spriteID to gun component.

--- plugins/components/gun/Gun.hpp
+++ plugins/components/gun/Gun.hpp
@@ -23,36 +23,31 @@
     #include <vector>
     #include <stdexcept>
     #include <libconfig.h++>
+    #include <string>
+    #include <cstring>
 
 /**
  * @brief Gun component class for ECS.
  * 
- * This class represents a gun component for an entity, storing bulletDamage and fire rate
- * values. It also provides methods to serialize and deserialize the gun data.
+ * This class represents a gun component for an entity, storing bulletDamage, fire rate,
+ * and bullet velocity values. It also provides methods to serialize and deserialize the gun data.
  */
 namespace Components {
 
-    /**
-     * @brief A component representing a gun with bulletDamage and fire rate attributes.
-     * 
-     * This class stores the gun's bulletDamage and fire rate, allowing it to be 
-     * serialized and deserialized for network transmission. It also provides 
-     * methods for initializing the component from configuration settings.
-     */
     class Gun : public AComponent<Gun> {
     public:
-        /**
-         * @brief Default constructor for the Gun component.
-         * 
-         * Initializes the bulletDamage, fire rate and bulletVelocity to default values.
-         */
-        Gun(uint32_t bulletDamage = 10, uint32_t fireRate = 500, uint32_t bulletVelocity = 8) : AComponent("Gun"), bulletDamage(bulletDamage), fireRate(fireRate), bulletVelocity(bulletVelocity), chrono() {}
+        Gun(
+            uint32_t bulletDamage = 10,
+            uint32_t fireRate = 500,
+            uint32_t bulletVelocity = 8,
+            std::string spriteId = "shot1") :
+            AComponent("Gun"),
+            bulletDamage(bulletDamage),
+            fireRate(fireRate),
+            bulletVelocity(bulletVelocity),
+            spriteId(spriteId),
+            chrono() {}
 
-        /**
-         * @brief Constructor that initializes the Gun component from a configuration.
-         * 
-         * @param config The configuration setting to extract the component data from.
-         */
         Gun(libconfig::Setting &config) : AComponent("Gun") {
             if (!config.lookupValue("bulletDamage", bulletDamage))
                 bulletDamage = 10;
@@ -60,12 +55,11 @@ namespace Components {
                 fireRate = 500;
             if (!config.lookupValue("bulletVelocity", bulletVelocity))
                 bulletVelocity = 8;
+            if (!config.lookupValue("spriteId", spriteId))
+                spriteId = "shot1";
             chrono.restart();
         }
 
-        /**
-         * @brief Default destructor for the Gun component.
-         */
         ~Gun() = default;
 
         /**
@@ -74,10 +68,23 @@ namespace Components {
          * @return A vector of bytes representing the serialized gun data.
          */
         std::vector<uint8_t> serialize() override {
-            __network.bulletDamage = htonl(*reinterpret_cast<uint32_t*>(&bulletDamage));
-            __network.fireRate = htonl(*reinterpret_cast<uint32_t*>(&fireRate));
-            __network.bulletVelocity = htonl(*reinterpret_cast<uint32_t*>(&bulletVelocity));
-            return std::vector<uint8_t>(__data, __data + sizeof(__data));
+            std::vector<uint8_t> data(16);
+
+            // Serialize bulletDamage, fireRate, and bulletVelocity with endianness conversion
+            uint32_t netBulletDamage = htonl(bulletDamage);
+            uint32_t netFireRate = htonl(fireRate);
+            uint32_t netBulletVelocity = htonl(bulletVelocity);
+
+            // Copy the 3 fields into the data array
+            std::memcpy(&data[0], &netBulletDamage, sizeof(uint32_t));
+            std::memcpy(&data[4], &netFireRate, sizeof(uint32_t));
+            std::memcpy(&data[8], &netBulletVelocity, sizeof(uint32_t));
+
+            // Limit spriteId to a maximum of 4 characters to fit within remaining space
+            std::string spriteIdLimited = spriteId.substr(0, 4);
+            std::memcpy(&data[12], spriteIdLimited.c_str(), spriteIdLimited.size());
+
+            return data;
         }
 
         /**
@@ -87,11 +94,21 @@ namespace Components {
          * @throws std::runtime_error If the data size is invalid.
          */
         void deserialize(std::vector<uint8_t> &data) override {
-            if (data.size() != sizeof(__data))
+            if (data.size() != 16)
                 throw std::runtime_error("Invalid data size for Gun component");
-            bulletDamage = ntohl(*reinterpret_cast<uint32_t *>(data.data()));
-            fireRate = ntohl(*reinterpret_cast<uint32_t *>(data.data() + 4));
-            bulletVelocity = ntohl(*reinterpret_cast<uint32_t *>(data.data() + 8));
+
+            // Deserialize bulletDamage, fireRate, and bulletVelocity with endianness conversion
+            std::memcpy(&bulletDamage, &data[0], sizeof(uint32_t));
+            bulletDamage = ntohl(bulletDamage);
+
+            std::memcpy(&fireRate, &data[4], sizeof(uint32_t));
+            fireRate = ntohl(fireRate);
+
+            std::memcpy(&bulletVelocity, &data[8], sizeof(uint32_t));
+            bulletVelocity = ntohl(bulletVelocity);
+
+            // Deserialize the spriteId (limited to 4 characters)
+            spriteId = std::string(data.begin() + 12, data.end());
         }
 
         /**
@@ -100,36 +117,27 @@ namespace Components {
          * @return The size of the data, in bytes.
          */
         size_t getSize() const override {
-            return sizeof(__data);
+            return 16; // 12 bytes for 3 uint32_t fields and 4 bytes for spriteId
         }
 
-        /**
-         * @brief Adds the Gun component to an entity.
-         * 
-         * @param to The entity to add the component to.
-         * @param engine The game engine.
-         * @param args The arguments to pass to the component constructor.
-         */
         void addTo(ECS::Entity &to, Engine::GameEngine &engine, std::vector<std::any> args) override {
-            if (args.size() != 2)
+            if (args.size() != 4)
                 throw std::runtime_error("Invalid number of arguments for Gun component");
+
             uint32_t bulletDamage = std::any_cast<uint32_t>(args[0]);
             uint32_t fireRate = std::any_cast<uint32_t>(args[1]);
             uint32_t bulletVelocity = std::any_cast<uint32_t>(args[2]);
-            engine.getRegistry().componentManager().addComponent<Components::Gun>(to, engine.newComponent<Components::Gun>(bulletDamage, fireRate, bulletVelocity));
+            std::string spriteId = std::any_cast<std::string>(args[3]);
+
+            engine.getRegistry().componentManager().addComponent<Components::Gun>(
+                to, engine.newComponent<Components::Gun>(bulletDamage, fireRate, bulletVelocity, spriteId));
         }
 
-        /**
-         * @brief Adds the Gun component to an entity from a configuration setting.
-         * 
-         * @param to The entity to add the component to.
-         * @param engine The game engine.
-         * @param config The configuration setting to extract the component data from.
-         */
         void addTo(ECS::Entity &to, Engine::GameEngine &engine, libconfig::Setting &config) override {
             int bulletDamage;
             int fireRate;
             int bulletVelocity;
+            std::string spriteId;
 
             if (!config.lookupValue("bulletDamage", bulletDamage))
                 bulletDamage = 10;
@@ -137,25 +145,21 @@ namespace Components {
                 fireRate = 500;
             if (!config.lookupValue("bulletVelocity", bulletVelocity))
                 bulletVelocity = 8;
-            std::unique_ptr<Components::Gun> gun = engine.newComponent<Components::Gun>(static_cast<uint32_t>(bulletDamage), static_cast<uint32_t>(fireRate), static_cast<uint32_t>(bulletVelocity));
+            if (!config.lookupValue("spriteId", spriteId))
+                spriteId = "shot1";
+
+            std::unique_ptr<Components::Gun> gun =
+                engine.newComponent<Components::Gun>(static_cast<uint32_t>(bulletDamage), static_cast<uint32_t>(fireRate),
+                                                     static_cast<uint32_t>(bulletVelocity), spriteId);
             engine.getRegistry().componentManager().addComponent<Components::Gun>(to, std::move(gun));
         }
 
         uint32_t bulletDamage;
         uint32_t fireRate;
         uint32_t bulletVelocity;
+        std::string spriteId;
         Chrono chrono;
-
-    private:
-        union {
-            struct {
-                uint32_t bulletDamage;
-                uint32_t fireRate;
-                uint32_t bulletVelocity;
-            } __network;
-            uint8_t __data[12];
-        };
     };
-};
+}
 
 #endif // GUN_HPP
