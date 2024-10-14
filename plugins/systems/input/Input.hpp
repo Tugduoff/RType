4c30edc1d487ca65fba96fb2b1a1cca14c5ce5a3
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-14 02:20:12 +0200

    :heavy_plus_sign: Added a debug to the function createProjectile.

--- plugins/systems/input/Input.hpp
+++ plugins/systems/input/Input.hpp
@@ -55,6 +55,8 @@ namespace Systems {
                 auto &reg = engine.getRegistry();
                 ECS::Entity projectile = reg.createEntity();
 
+                std::cerr << "New entity for projectile" << projectile << std::endl;
+
                 std::unique_ptr<Components::Position> positionComponent =
                     std::make_unique<Components::Position>(posX, posY, 1);
                 reg.componentManager().addComponent(projectile, std::move(positionComponent));
