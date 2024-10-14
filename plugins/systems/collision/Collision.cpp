4875fd3de28f15d584ee0b8db0dfe9d0faf7a1c1
Tugdual de Reviers <tugdual.de-reviers@epitech.eu>
2024-10-14 04:08:20 +0200

    :heavy_plus_sign: Added enemy projectiles to player collision.

--- plugins/systems/collision/Collision.cpp
+++ plugins/systems/collision/Collision.cpp
@@ -25,61 +25,8 @@ void Systems::Collision::run(Engine::GameEngine &engine)
     auto &reg = engine.getRegistry();
 
     try {
-        auto &spriteIdComponents = reg.componentManager().getComponents<Components::SpriteIDComponent>();
-        auto &posComponents = reg.componentManager().getComponents<Components::Position>();
-        auto &colliderComponents = reg.componentManager().getComponents<Components::Collider>();
-        auto &damageComponents = reg.componentManager().getComponents<Components::Damage>();
-        auto &healthComponents = reg.componentManager().getComponents<Components::Health>();
-
-        std::vector<ECS::Entity> enemies;
-        std::vector<ECS::Entity> projectiles;
-
-        size_t i = 0;
-        for (i = 0; i < spriteIdComponents.size(); i++) {
-            try {
-                auto &spriteId = spriteIdComponents[i];
-
-                if (spriteId->id == Components::SpriteID::Enemy) {
-                    enemies.push_back((ECS::Entity)i);
-                } else if (spriteId->id == Components::SpriteID::ProjectileRight) {
-                    projectiles.push_back((ECS::Entity)i);
-                }
-            } catch (std::exception &e) {
-                std::cerr << "Error: " << e.what() << std::endl;
-                continue;
-            }
-        }
-        for (auto &proj : projectiles) {
-            for (auto &enemy : enemies) {
-                try {
-                    std::cerr << "Checking collision between projectile " << proj << " and enemy " << enemy << std::endl;
-                    auto &projPos = posComponents[proj];
-                    auto &projCollider = colliderComponents[proj];
-                    auto &enemyPos = posComponents[enemy];
-                    auto &enemyCollider = colliderComponents[enemy];
-                    std::cerr << "Values are for proj : " << projPos->x << " " << projPos->y << " " << projCollider->width << " " << projCollider->height << std::endl;
-                    std::cerr << "Values are for enemy : " << enemyPos->x << " " << enemyPos->y << " " << enemyCollider->width << " " << enemyCollider->height << std::endl;
-                    if (projPos->x - projCollider->width / 2 < enemyPos->x + enemyCollider->width / 2 &&
-                        projPos->x + projCollider->width / 2 > enemyPos->x - enemyCollider->width / 2 &&
-                        projPos->y - projCollider->height / 2 < enemyPos->y + enemyCollider->height / 2 &&
-                        projPos->y + projCollider->height / 2 > enemyPos->y - enemyCollider->height / 2) {
-                        auto &projDamage = damageComponents[proj];
-                        auto &enemyHealth = healthComponents[enemy];
-
-                        enemyHealth->currentHealth -= projDamage->damage;
-
-                        std::cerr << "Projectile hit enemy. Enemy health: " << enemyHealth->currentHealth << std::endl;
-                        if (enemyHealth->currentHealth <= 0) {
-                            reg.killEntity(enemy);
-                        }
-                        reg.killEntity(proj);
-                    }
-                } catch (std::exception &e) {
-                    std::cerr << "Collision Error: " << e.what() << std::endl;
-                    continue;
-                }
-            }
-        }
+        checkPlayerProjectileToEnemyCollision(reg);
+        checkEnemyProjectileToPlayerCollision(reg);
     } catch (std::runtime_error &e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
@@ -109,4 +56,120 @@ LIBRARY_ENTRYPOINT
 Systems::ISystem *entryConfig(libconfig::Setting &config)
 {
     return new Systems::Collision(config);
+}
+
+void Systems::Collision::checkPlayerProjectileToEnemyCollision(ECS::Registry &reg) {
+    auto &spriteIdComponents = reg.componentManager().getComponents<Components::SpriteIDComponent>();
+    auto &posComponents = reg.componentManager().getComponents<Components::Position>();
+    auto &colliderComponents = reg.componentManager().getComponents<Components::Collider>();
+    auto &damageComponents = reg.componentManager().getComponents<Components::Damage>();
+    auto &healthComponents = reg.componentManager().getComponents<Components::Health>();
+
+    std::vector<ECS::Entity> enemies;
+    std::vector<ECS::Entity> projectiles;
+
+    size_t i = 0;
+    for (i = 0; i < spriteIdComponents.size(); i++) {
+        try {
+            auto &spriteId = spriteIdComponents[i];
+
+            if (spriteId->id == Components::SpriteID::Enemy) {
+                enemies.push_back((ECS::Entity)i);
+            } else if (spriteId->id == Components::SpriteID::ProjectileRight) {
+                projectiles.push_back((ECS::Entity)i);
+            }
+        } catch (std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            continue;
+        }
+    }
+    for (auto &proj : projectiles) {
+        for (auto &enemy : enemies) {
+            try {
+                std::cerr << "Checking collision between projectile " << proj << " and enemy " << enemy << std::endl;
+                auto &projPos = posComponents[proj];
+                auto &projCollider = colliderComponents[proj];
+                auto &enemyPos = posComponents[enemy];
+                auto &enemyCollider = colliderComponents[enemy];
+                std::cerr << "Values are for proj : " << projPos->x << " " << projPos->y << " " << projCollider->width << " " << projCollider->height << std::endl;
+                std::cerr << "Values are for enemy : " << enemyPos->x << " " << enemyPos->y << " " << enemyCollider->width << " " << enemyCollider->height << std::endl;
+                if (projPos->x - projCollider->width / 2 < enemyPos->x + enemyCollider->width / 2 &&
+                    projPos->x + projCollider->width / 2 > enemyPos->x - enemyCollider->width / 2 &&
+                    projPos->y - projCollider->height / 2 < enemyPos->y + enemyCollider->height / 2 &&
+                    projPos->y + projCollider->height / 2 > enemyPos->y - enemyCollider->height / 2) {
+                    auto &projDamage = damageComponents[proj];
+                    auto &enemyHealth = healthComponents[enemy];
+
+                    enemyHealth->currentHealth -= projDamage->damage;
+
+                    std::cerr << "Projectile hit enemy. Enemy health: " << enemyHealth->currentHealth << std::endl;
+                    if (enemyHealth->currentHealth <= 0) {
+                        reg.killEntity(enemy);
+                    }
+                    reg.killEntity(proj);
+                }
+            } catch (std::exception &e) {
+                std::cerr << "Collision Error: " << e.what() << std::endl;
+                continue;
+            }
+        }
+    }
+}
+
+void Systems::Collision::checkEnemyProjectileToPlayerCollision(ECS::Registry &reg) {
+    auto &spriteIdComponents = reg.componentManager().getComponents<Components::SpriteIDComponent>();
+    auto &posComponents = reg.componentManager().getComponents<Components::Position>();
+    auto &colliderComponents = reg.componentManager().getComponents<Components::Collider>();
+    auto &damageComponents = reg.componentManager().getComponents<Components::Damage>();
+    auto &healthComponents = reg.componentManager().getComponents<Components::Health>();
+
+    std::vector<ECS::Entity> players;
+    std::vector<ECS::Entity> projectiles;
+
+    size_t i = 0;
+    for (i = 0; i < spriteIdComponents.size(); i++) {
+        try {
+            auto &spriteId = spriteIdComponents[i];
+
+            if (spriteId->id == Components::SpriteID::Player) {
+                players.push_back((ECS::Entity)i);
+            } else if (spriteId->id == Components::SpriteID::ProjectileLeft) {
+                projectiles.push_back((ECS::Entity)i);
+            }
+        } catch (std::exception &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+            continue;
+        }
+    }
+    for (auto &proj : projectiles) {
+        for (auto &player : players) {
+            try {
+                std::cerr << "Checking collision between projectile " << proj << " and player " << player << std::endl;
+                auto &projPos = posComponents[proj];
+                auto &projCollider = colliderComponents[proj];
+                auto &playerPos = posComponents[player];
+                auto &playerCollider = colliderComponents[player];
+                std::cerr << "Values are for proj : " << projPos->x << " " << projPos->y << " " << projCollider->width << " " << projCollider->height << std::endl;
+                std::cerr << "Values are for player : " << playerPos->x << " " << playerPos->y << " " << playerCollider->width << " " << playerCollider->height << std::endl;
+                if (projPos->x - projCollider->width / 2 < playerPos->x + playerCollider->width / 2 &&
+                    projPos->x + projCollider->width / 2 > playerPos->x - playerCollider->width / 2 &&
+                    projPos->y - projCollider->height / 2 < playerPos->y + playerCollider->height / 2 &&
+                    projPos->y + projCollider->height / 2 > playerPos->y - playerCollider->height / 2) {
+                    auto &projDamage = damageComponents[proj];
+                    auto &playerHealth = healthComponents[player];
+
+                    playerHealth->currentHealth -= projDamage->damage;
+
+                    std::cerr << "Projectile hit player. Player health: " << playerHealth->currentHealth << std::endl;
+                    if (playerHealth->currentHealth <= 0) {
+                        reg.killEntity(player);
+                    }
+                    reg.killEntity(proj);
+                }
+            } catch (std::exception &e) {
+                std::cerr << "Collision Error: " << e.what() << std::endl;
+                continue;
+            }
+        }
+    }
 }
--- plugins/systems/collision/Collision.hpp
+++ plugins/systems/collision/Collision.hpp
@@ -32,6 +32,24 @@ namespace Systems {
             void run(Engine::GameEngine &engine) override;
             void init(Engine::GameEngine &engine) override;
         private:
+
+            /**
+             * @brief Check the collision between player projectiles and enemies.
+             * 
+             * @param reg The registry of the game engine
+             * 
+             * @note This function will check the collision between player projectiles and enemies.
+             */
+            void checkPlayerProjectileToEnemyCollision(ECS::Registry &reg);
+
+            /**
+             * @brief Check the collision between enemy projectiles and the player.
+             * 
+             * @param reg The registry of the game engine
+             * 
+             * @note This function will check the collision between enemy projectiles and the player.
+             */
+            void checkEnemyProjectileToPlayerCollision(ECS::Registry &reg);
     };
 };
 
