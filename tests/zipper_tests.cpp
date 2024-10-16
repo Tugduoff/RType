7c930b255713efe99fe58c39a08a1dc531c47706
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-16 10:52:59 +0200

    :test_tube: Added failing test for zipper

--- tests/zipper_tests.cpp
+++ tests/zipper_tests.cpp
@@ -9,21 +9,21 @@
 
 using std::nullopt;
 
+static const std::array<std::function<void(int, char)>, 4> expected1 = {
+    ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 1); }),
+    ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 2); }),
+    ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 3); }),
+    ([](int, char) { cr_assert_fail("Zipper out of range"); }),
+};
+
 Test(zipper_tests, basic_zipper_test)
 {
     std::vector<std::optional<int>> v1 = {1, nullopt, 2, 84, nullopt, nullopt, 3};
     std::vector<std::optional<char>> v2 = {1, 84, 2, nullopt, 84, nullopt, 3};
 
-    std::array<std::function<void(int, char)>, 4> expected = {
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 1); }),
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 2); }),
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 3); }),
-        ([](int, char) { cr_assert_fail("Zipper out of range"); }),
-    };
-
     unsigned i = 0;
     for (auto &&[x1, x2] : Zipper(v1, v2)) {
-        expected[i](x1, x2);
+        expected1[i](x1, x2);
         i++;
     }
 }
@@ -43,16 +43,21 @@ Test(zipper_tests, unique_zipper)
         v2.push_back(std::unique_ptr<char>(x));
     }
 
-    std::array<std::function<void(int, char)>, 4> expected = {
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 1); }),
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 2); }),
-        ([](int x1, char x2) { cr_assert(x1 == x2 && x1 == 3); }),
-        ([](int, char) { cr_assert_fail("Zipper out of range"); }),
-    };
+    unsigned i = 0;
+    for (auto &&[x1, x2] : Zipper(v1, v2)) {
+        expected1[i](x1, x2);
+        i++;
+    }
+}
+
+Test(zipper_tests, zipper_with_first_invalid)
+{
+    std::vector<std::optional<int>> v1 = {nullopt, 1, nullopt, 2, 84, nullopt, nullopt, 3};
+    std::vector<std::optional<char>> v2 = {84, 1, 84, 2, nullopt, 84, nullopt, 3};
 
     unsigned i = 0;
     for (auto &&[x1, x2] : Zipper(v1, v2)) {
-        expected[i](x1, x2);
+        expected1[i](x1, x2);
         i++;
     }
 }
