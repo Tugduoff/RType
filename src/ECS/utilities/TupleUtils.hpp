63a860351e4537734f7859ed88b5aa6654c020c6
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-21 15:25:58 +0200

    :memo: Added documentation for TupleUtils.hpp

--- src/ECS/utilities/TupleUtils.hpp
+++ src/ECS/utilities/TupleUtils.hpp
@@ -10,17 +10,37 @@
 
     #include <tuple>
 
+/**
+ * @brief Helper type used to declare tuple types that
+ * @brief result from concatenating two or more tuples
+ *
+ * @tparam tups The tuple types to concatenate
+ */
 template <typename... tups>
-using NtupleCombination = decltype(std::tuple_cat(std::declval<tups>()...));
+using NTupleCombination = decltype(std::tuple_cat(std::declval<tups>()...));
 
+/**
+ * @brief Prepends zero or more types to the start
+ * @brief of the type list of a tuple type
+ *
+ * @tparam Tup The tuple type to prepend types to
+ * @tparam types The types to prepend to the tuple
+ */
 template <typename Tup, typename... types>
-using prependTupleType = NtupleCombination<
+using prependTupleType = NTupleCombination<
     decltype(std::make_tuple(std::declval<types>()...)),
     decltype(std::declval<Tup>())
 >;
 
+/**
+ * @brief Appends zero or more types to the end
+ * @brief of the type list of a tuple type
+ *
+ * @tparam Tup The tuple type to append types to
+ * @tparam types The types to append to the tuple
+ */
 template <typename Tup, typename... types>
-using appendTupleType = NtupleCombination<
+using appendTupleType = NTupleCombination<
     decltype(std::declval<Tup>()),
     decltype(std::make_tuple(std::declval<types>()...))
 >;
