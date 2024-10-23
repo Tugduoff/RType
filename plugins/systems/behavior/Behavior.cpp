cd999727fdd1d80c5d76bffdf9c65953731ee7ee
tibaud le maout <tibaud.le-maout@epitech.eu>
2024-10-23 10:49:40 +0200

    :heavy_plus_sign: Added behavior for going up and down repeatedly

--- plugins/systems/behavior/Behavior.cpp
+++ plugins/systems/behavior/Behavior.cpp
@@ -24,19 +24,31 @@ void Systems::BehaviorSystem::run(Engine::GameEngine &engine)
     
     try {
         auto &aiComponent = reg.componentManager().getComponents<Components::Ai>();
-        auto &aiComponent = reg.componentManager().getComponents<Components::Position>();
+        auto &posComponent = reg.componentManager().getComponents<Components::Position>();
+        auto &velComponent = reg.componentManager().getComponents<Components::Velocity>();
+        auto &deathRangeComponent = reg.componentManager().getComponents<Components::DeathRange>();
 
         size_t i = 0;
         try {
-            for (i = 0; i < aiComponent.size(); i++) {
+            for (i = 0; i < aiComponent.size() || i < posComponent.size() || i < velComponent.size() || i < deathRangeComponent.size(); i++) {
                 auto &aiBehavior = aiComponent[i];
+                auto &posBehavior = posComponent[i];
+                auto &velBehavior = velComponent[i];
+                auto &deathRangeBehavior = deathRangeComponent[i];
+
                 if (aiBehavior->behavior == 0)
                     continue;
                 if (aiBehavior->behavior == 1) {
-                    std::cerr << "Entity " << i << " has behavior 1" << std::endl;   
+                    std::cerr << "Entity " << i << " has behavior 1" << std::endl;
+                    velBehavior->floatX = 0;
+                    if (posBehavior->y > deathRangeBehavior->minY + 50)
+                        velBehavior->floatY = 5;
+                    else if (posBehavior->y < deathRangeBehavior->maxY - 50)
+                        velBehavior->floatY = -5;
+                    engine.updateComponent(i, velBehavior->getId(), velBehavior->serialize());
                 }
                 if (aiBehavior->behavior == 2) {
-                    std::cerr << "Entity " << i << " has behavior 2" << std::endl;   
+                    std::cerr << "Entity " << i << " has behavior 2" << std::endl;
                 }
             }
         } catch (std::exception &e) {
@@ -52,7 +64,9 @@ void Systems::BehaviorSystem::init(Engine::GameEngine &engine)
     if (!engine.registerComponent<Components::Ai>("./plugins/bin/components/", "Ai"))
         std::cerr << "Error: Could not register Ai component in system Ai" << std::endl;
     if (!engine.registerComponent<Components::Position>("./plugins/bin/components/", "Postion"))
-        std::cerr << "Error: Could not register Postion component in system Ai" << std::endl;
+        std::cerr << "Error: Could not register Position component in system Ai" << std::endl;
+    if (!engine.registerComponent<Components::Velocity>("./plugins/bin/components/", "Velocity"))
+        std::cerr << "Error: Could not register Velocity component in system Ai" << std::endl;
 }
 
 LIBRARY_ENTRYPOINT
