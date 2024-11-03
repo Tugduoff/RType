baf4d0eacc15573e80510dfb1977852dfe55252b
Florent Charpentier <florent.charpentier@epitech.eu>
2024-11-03 09:22:13 +0100

    :fire: Remove hard-coded `engine.registerComponent` in Client

--- src/client/Main.cpp
+++ src/client/Main.cpp
@@ -34,19 +34,6 @@ int main()
     Chrono chrono;
 
     engine.loadSystems("./src/client/configClient.cfg");
-    // Hard coded register for now
-    engine.registerComponent<Components::Visible>("./plugins/bin/components/", "Visible");
-    engine.registerComponent<Components::Health>("./plugins/bin/components/", "Health");
-    engine.registerComponent<Components::Collider>("./plugins/bin/components/", "Collider");
-    engine.registerComponent<Components::Velocity>("./plugins/bin/components/", "Velocity");
-    engine.registerComponent<Components::Acceleration>("./plugins/bin/components/", "Acceleration");
-    engine.registerComponent<Components::Damage>("./plugins/bin/components/", "Damage");
-    engine.registerComponent<Components::Controllable>("./plugins/bin/components/", "Controllable");
-    engine.registerComponent<Components::Gun>("./plugins/bin/components/", "Gun");
-    engine.registerComponent<Components::Type>("./plugins/bin/components/", "Type");
-    engine.registerComponent<Components::Position>("./plugins/bin/components/", "Position");
-    engine.registerComponent<Components::SpriteID>("./plugins/bin/components/", "SpriteID");
-    engine.registerComponent<Components::DeathRange>("./plugins/bin/components/", "DeathRange");
 
     conn.engineInit(engine.getIdStringToType());
     conn.asyncReceive(engine);
